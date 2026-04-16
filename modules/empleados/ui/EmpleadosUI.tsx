132bf9d4ffca41f37239efcb9cd2cc42134c556d
Amaya2711 <juan.manuel.amaya.suarez@gmail.com>
2026-04-16 04:18:33 -0500

    Subir todos los cambios

--- modules/empleados/ui/EmpleadosUI.tsx
+++ modules/empleados/ui/EmpleadosUI.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Empleado } from "../../domain/empleado";
+import { Empleado } from "../domain/empleado";
 import { getEmpleados, addEmpleado, updateEmpleado, inactivateEmpleado } from "../infrastructure/empleado-api";
 
 export function EmpleadosUI() {
